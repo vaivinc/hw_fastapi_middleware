11dc90ee3107bd2c91447f68e7105c5e964590ec
vaivinc <168576062+vaivinc@users.noreply.github.com>
2024-12-02 15:28:55 +0100

    Update routers.py

--- routers.py
+++ routers.py
@@ -1,3 +1,14 @@
 from fastapi import APIRouter
 
+
 route = APIRouter()
+
+
+@route.post("/")
+async def create_user():
+    ...
+
+
+@route.get("/read/all/")
+async def all_users():
+    return {"User": "Anonimus"}
